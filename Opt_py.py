7968d9dea9bbdd9198ce28699cf824e9fee49f71
RacemMELLOULI <racem.mellouli@fsegs.usf.tn>
2024-11-06 10:38:52 +0100

    Update Opt_py.py

--- Opt_py.py
+++ Opt_py.py
@@ -1,3 +1,12 @@
+#### Capacitated Vehicle Routing Problem 
+#### with a Heterogeneous Vehicle Fleet and Restrictions on the Number of Vehicles Utilized 
+#### (H-CVRP-MV)
+#### Dr. Racem Mellouli - M2 GOL (Software Applications for Operations Optimization)
+
+# Note: There exist various vehicle usage restrictions, such as daily mileage limits 
+# and workday duration, in addition to vehicle capacity. Specifically, this model focuses on 
+# limiting the number of vehicles utilized among the available fleet.
+
 import os
 import sys
 import subprocess
