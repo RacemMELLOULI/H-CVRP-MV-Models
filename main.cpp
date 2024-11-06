e9dc36d458c3ce2f1cc9e674297edb866b7462e3
RacemMELLOULI <racem.mellouli@fsegs.usf.tn>
2024-11-06 10:39:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+///// Capacitated Vehicle Routing Problem 
+///// with a Heterogeneous Vehicle Fleet and Restrictions on the Number of Vehicles Utilized 
+///// (H-CVRP-MV)
+///// Dr. Racem Mellouli - M2 GOL (Software Applications for Operations Optimization)
+
+// Note: There exist various vehicle usage restrictions, such as daily mileage limits 
+// and workday duration, in addition to vehicle capacity. Specifically, this model focuses on 
+// limiting the number of vehicles utilized among the available fleet.
+
+
 #include <ilcplex/ilocplex.h>
 #include <iostream>
 ILOSTLBEGIN
